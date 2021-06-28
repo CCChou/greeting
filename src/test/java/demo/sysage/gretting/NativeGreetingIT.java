017a5adb399fb968661a7c392463498c36962267
dennischou <dses5900@gmail.com>
2021-06-28 15:30:12 +0800

    Remove useless code for demo

--- src/test/java/demo/sysage/gretting/NativeGreetingIT.java
+++
@@ -1,9 +0,0 @@
-package demo.sysage.gretting;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingIT extends GreetingTest {
-
-    // Execute the same tests but in native mode.
-}
